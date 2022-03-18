d08482e39345872e7cee2db9a6b004d5b6f875ab
rbbostelmann <rodrigobostelmann@gmail.com>
2022-03-18 12:57:30 -0300

    Buttons added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("canvas")
 let tiles;
 
+let blackBtn = document.getElementById("black-btn")
+let rainbowBtn = document.getElementById("rainbow-btn")
+let eraserBtn = document.getElementById("eraser-btn")
+let clearBtn = document.getElementById("clear-btn")
+
 
 //Populates the canvas with the tiles
 window.onload = (e) => {
@@ -16,6 +21,27 @@ window.onload = (e) => {
     }
 }
 
+//Event listener for the black button to start drawing
+blackBtn.addEventListener("click", e => {
+    draw()
+})
+
+//Event listener for the rainbow button to start drawing
+rainbowBtn.addEventListener("click", e => {
+    rainbow()
+})
+
+//Event listener for the black button to start drawing
+eraserBtn.addEventListener("click", e => {
+    erase()
+})
+
+//Event listener for the black button to start drawing
+clearBtn.addEventListener("click", e => {
+    clear()
+})
+
+
 //Draw function with mouse over
 function draw() {
     for (let i = 0; i <= 255; i++) {
@@ -28,12 +54,34 @@ function draw() {
     }
 }
 
+//
+function rainbow() {
+    tiles[2].addEventListener("mouseover", () => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+
+    })
+}
+
+
+
 //Eraser function with mouse over
-function eraser() {
+function erase() {
     for (let i = 0; i <= 255; i++) {
         tiles[i].addEventListener("mouseover", () => {
             tiles[i].classList.remove("on")
+            tiles[i].classList.remove("rainbow")
             tiles[i].classList.add("off")
         })
+    }
+}
+
+//Clear function that clears the canvas
+function clear() {
+    for (let i = 0; i <= 255; i++) {
+        tiles[i].classList.remove("on")
+        tiles[i].classList.remove("rainbow")
+        tiles[i].classList.add("off")
+
     }
 }
